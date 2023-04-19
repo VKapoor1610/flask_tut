73f27b791f53f4b94beb5acc9200ff4db72b65a3
Replit user <>
2023-04-19 16:17:34 +0000

    Dyanamic data is added to app

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello_world():
-#   return "HELLO Vihan"
-
-# if __name__ == '__main__' : 
-#   app.run(host='0.0.0.0' , debug=True)
-
-from flask import Flask
-# import the Flask class from flask module 
+from flask import Flask, render_template, jsonify
+# import the Flask class from flask module
 
 # then create an app object of type('Flask')
 app = Flask(__name__)
 
+data = ['Vihan', 'Kapoor', 'Payal']
+
+# data = [{} , {]
+
+
 # set the route of the app
-@app.route("/") #@ is decorator function 
+@app.route("/")  #@ is decorator function
 def func():
-  return "Hello" #just below the route wrote the corresponding function 
+  return render_template(
+    'home.html',
+    data=data)  #just below the route wrote the corresponding function
 
-# way to run a flask program 
-if __name__ == "__main__" : 
-  app.run(host= '0.0.0.0' , debug=True)
+@app.route("/api/user_info")
+def fun1():
+  return jsonify(data)
+# for sending data in form of data variable we use this format
 
-
-
-
+# way to run a flask program
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
